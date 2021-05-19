21afa71c22274692c69aef0b5993cb69db14d00a
Matthew Yorkgitis <myorkgitis@gmail.com>
2021-05-19 10:30:03 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to run this framework
+
+## NextJS Dev
+
+
+## Firebase Deployment
---
+++ app/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	distDir: "nextjs",
+	env: {
+		FIREBASE_PROJECT_ID: "test-ts-nextjs-firebase",
+	},
+	experimental: {
+		sprFlushToDisk: false,
+	},
+};
